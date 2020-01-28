25720852b0032cf376c7578eef4cba326ebcbaa9
EWAN FAWNS <1802650@uad.ac.uk>
2020-01-28 13:43:46 +0000

    Place basic assets
    placed in the player and ground assets, then added collision and rigidbodies

---
+++ Group7Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+         
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Moveplayer();
+	}
+
+    void Moveplayer()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+
+        }
+    }
+}
