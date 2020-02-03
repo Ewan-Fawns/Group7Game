cce0ac39fbb4387d2b891964156d466d1c3f2046
Ewan-Fawns <59920488+Ewan-Fawns@users.noreply.github.com>
2020-02-03 21:46:11 +0000

    Ladders
    Implemented ladders and the climbing ladders mechanic

--- Group7Game/Assets/Scripts/PlayerController.cs
+++ Group7Game/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour {
     public bool isMovingStone = false; // used to check to see if the player is moving a rune stone
     public int checkPoint = 0; // used to check which checkpoint the player is at
 
+    private bool isOnLadder = false;
     private bool isOnGround = false; // used to check if the player is on the ground
     private GameObject interactable = null;//used to check which game object is currently selected for interaction
     
@@ -33,28 +34,28 @@ public class PlayerController : MonoBehaviour {
     //used to move the player
     void MovePlayer()
     {
-        
-        //move left
-        if (Input.GetKey(KeyCode.A))
+        if(isOnLadder == false)
         {
-            rb.AddForce(Vector3.left * speed);
-        }
-        //move right
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(Vector3.right * speed);
-        }
-        //jump
-        if (Input.GetKey(KeyCode.W))
-        {
-            if(isOnGround == true && isMovingStone == false)
+            //move left
+            if (Input.GetKey(KeyCode.A))
             {
-                rb.velocity = new Vector3(rb.velocity.x, jumpHeight, 0);
-                isOnGround = false;
+                rb.AddForce(Vector3.left * speed);
             }
-        }
-
-        //Used to limit speed
+            //move right
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.AddForce(Vector3.right * speed);
+            }
+            //jump
+            if (Input.GetKey(KeyCode.W))
+            {
+                if (isOnGround == true && isMovingStone == false)
+                {
+                    rb.velocity = new Vector3(rb.velocity.x, jumpHeight, 0);
+                    isOnGround = false;
+                }
+            }
+            //Used to limit speed
         if (rb.velocity.x > 10.0f)
         {
             rb.velocity = new Vector3(10.0f, rb.velocity.y, 0);
@@ -63,6 +64,22 @@ public class PlayerController : MonoBehaviour {
         {
             rb.velocity = new Vector3(-10.0f, rb.velocity.y, 0);
         }
+        }
+        else
+        {
+            
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += new Vector3(0, 1, 0) * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += new Vector3(0, -1, 0) * Time.deltaTime;
+            }
+        }
+        
+
+        
        
     }
 
@@ -100,6 +117,21 @@ public class PlayerController : MonoBehaviour {
                     isMovingStone = false;
                 }
             }
+            else if(interactable.tag == "Ladder")
+            {
+                if(isOnLadder == false)
+                {
+                    isOnLadder = true;
+                    rb.velocity = new Vector3(0, 0, 0);
+                    rb.gravityScale = 0f;
+                    transform.position = new Vector3(interactable.transform.position.x, transform.position.y, 0);
+                }
+                else
+                {
+                    isOnLadder = false;
+                    rb.gravityScale = 1.5f;
+                }
+            }
         }
     }
 
@@ -108,6 +140,30 @@ public class PlayerController : MonoBehaviour {
         if(collision.gameObject.tag == "CheckPoint")
         {
             checkPoint = collision.GetComponent<CheckPoint>().checkPointNum;
+        }
+
+        if (collision.gameObject.tag == "Ladder" && isMovingStone == false)
+        {
+            interactable = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ladder" && isMovingStone == false)
+        {
+            interactable = null;
+            if(isOnLadder == true && transform.position.y > collision.transform.position.y)
+            {
+                rb.gravityScale = 1.5f;
+                rb.velocity = new Vector3(rb.velocity.x, 5f, 0);
+                isOnLadder = false;
+            }
+            else if(isOnLadder == true && transform.position.y < collision.transform.position.y)
+            {
+                rb.gravityScale = 1.5f;
+                isOnLadder = false;
+            }
         }
     }
 
@@ -149,5 +205,10 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    
+    void createRigidbody()
+    {
+        rb = gameObject.AddComponent<Rigidbody2D>();
+        rb.mass = 0.5f;
+        rb.gravityScale = 1.5f;
+    }
 }
