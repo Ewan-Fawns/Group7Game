b6a6ba0988a13b83284bfc5bdd39e6ea6afa7f4e
StuartwR <43067361+StuartwR@users.noreply.github.com>
2020-02-04 16:29:50 +0000

    Ball with Gravtity and Rotating Platforms (WIP)
    Add a ball with gravity, and platforms that rotate using F and G, trying to get the platforms to only rotate when the player is standing on the blue rectangle on the ground next to them. Any fixes/sollutions would be appreciated.

---
+++ Group7Game/Assets/Scripts/RotationTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTrigger : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            RotatingPlatform myScript = other.gameObject.GetComponent<RotatingPlatform>();
+
+            myScript.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            RotatingPlatform myScript = other.gameObject.GetComponent<RotatingPlatform>();
+            myScript.enabled = false;
+        }
+    }
+}
+
+// THIS SCRIPT DOESN'T WORK YET
