3b9390776dcb86d66374af172acf8184bcbc51ea
Ewan-Fawns <59920488+Ewan-Fawns@users.noreply.github.com>
2020-02-08 12:01:50 +0000

    Code improvements
    Made some code improvements to allow the dragging functionality to be inherited by other classes, rather than just having it be used in the RuneStone class. Should speed up development time, should we have to implement dragging in other classes.

---
+++ Group7Game/Assets/Scripts/DraggedObject.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggedObject : MonoBehaviour {
+    private bool playerCollision = false; //used to prevent the rigidbody from being destroyed if the player is dragging it
+    private bool isTouchingStone = false; //used to prevent bugs where collisions break the joint
+    private bool isTouchingOtherObject = false; //used to prevent bugs where collisions break the joint
+    public Rigidbody2D rb;//Rigidbody is dynamically created under certain conditions
+    public DistanceJoint2D distanceJoint; //Distance joint to be added to the player when dragging
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = gameObject.AddComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        //limits the movement of the rune stone
+        if (rb != null)
+        {
+            if (rb.velocity.x > 1)
+            {
+                rb.velocity = new Vector3(1f, rb.velocity.y, 0);
+            }
+            else if (rb.velocity.x < -1)
+            {
+                rb.velocity = new Vector3(-1f, rb.velocity.y, 0);
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //destroys the rigidbody when colliding with the ground, but only if the player isnt touching the collider
+        if (collision.gameObject.tag == "Ground")
+        {
+            if (playerCollision == false && GameObject.Find("Character").GetComponent<PlayerController>().isMovingStone == false)
+            {
+                Destroy(rb);
+            }
+        }
+        //ensures the rune stone does not break the joint when colliding with another rune stone
+        if (collision.gameObject.tag == "RuneStone")
+        {
+            isTouchingOtherObject = true;
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //stops the joint from being destroyed if the player
+        if (collision.gameObject.tag == "Player")
+        {
+            playerCollision = false;
+        }
+
+        //makes the stone fall when pushed off a ledge and prevents the joint from breaking if you collide with another stone
+        if (collision.gameObject.tag == "Ground" && isTouchingOtherObject == false)
+        {
+            Destroy(distanceJoint);
+            GameObject.Find("Character").GetComponent<PlayerController>().isMovingStone = false;
+        }
+        //makes it possible for the rune stone to break the joint after falling off edge, should the player touch the rune stone with another rune stone
+        if (collision.gameObject.tag == "RuneStone")
+        {
+            isTouchingOtherObject = false;
+        }
+    }
+}
--- Group7Game/Assets/Scripts/RuneStone.cs
+++ Group7Game/Assets/Scripts/RuneStone.cs
@@ -2,12 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RuneStone : MonoBehaviour {
+public class RuneStone : DraggedObject {
     private Vector3 origonalLocation; //Used to store the origonal location of the rune stone for teleportation
-    private bool playerCollision = false; //used to prevent the rigidbody from being destroyed if the player is dragging it
-    private bool isTouchingStone = false; //used to prevent bugs where collisions break the joint
-    public Rigidbody2D rb;//Rigidbody is dynamically created under certain conditions
-    public DistanceJoint2D distanceJoint; //Distance joint to be added to the player when dragging
     public int identifier = 0; //identifier used to check if the rune stone is associated with the rune stone slot
     public int checkPoint = 0; //used to identify which checkpoint is associated with this runestone
 
@@ -39,24 +35,7 @@ public class RuneStone : MonoBehaviour {
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //destroys the rigidbody when colliding with the ground, but only if the player isnt touching the collider
-        if(collision.gameObject.tag == "Ground")
-        {
-            if(playerCollision == false && GameObject.Find("Character").GetComponent<PlayerController>().isMovingStone == false)
-            {
-              Destroy(rb);
-                print("jup");
-            }
-        }
-        //ensures the rune stone does not break the joint when colliding with another rune stone
-        if (collision.gameObject.tag == "RuneStone")
-        {
-            isTouchingStone = true;
-        }
 
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         /*if the rune stone identifier matches the rune stone slots identifier, the rune stone will fit into the rune stone slot, otherwise it will be teleported back to its origonal location, 
@@ -75,29 +54,7 @@ public class RuneStone : MonoBehaviour {
             else
             {
                 transform.position = origonalLocation;
-
             }
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-
-        //stops the joint from being destroyed if the player
-        if (collision.gameObject.tag == "Player")
-        {
-            playerCollision = false;
-        }
-
-        //makes the stone fall when pushed off a ledge and prevents the joint from breaking if you collide with another stone
-        if(collision.gameObject.tag == "Ground" && isTouchingStone == false)
-        {
-            Destroy(distanceJoint);
-            GameObject.Find("Character").GetComponent<PlayerController>().isMovingStone = false;
-        }
-        //makes it possible for the rune stone to break the joint after falling off edge, should the player touch the rune stone with another rune stone
-        if (collision.gameObject.tag == "RuneStone")
-        {
-            isTouchingStone = false;
         }
     }
 
